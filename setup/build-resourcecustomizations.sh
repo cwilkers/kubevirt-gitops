a5e79b3ca5a8e4281447f41697abee3538915117
cwilkers <cwilkers@redhat.com>
2021-07-19 13:14:16 -0500

    Refactored to make adding more resources easier

--- setup/build-resourcecustomizations.sh
+++ setup/build-resourcecustomizations.sh
@@ -5,10 +5,10 @@ ARGO_NS=${A[0]}
 ARGO_CR=${A[1]}
 
 
-DV_YAML=$(sed 's/^/        /' datavolumes.health.lua)
+DV_YAML=$(jj)
 VM_YAML=$(sed 's/^/        /' virtualmachines.health.lua)
 
-read -r -d '' YAML <<EOF
+cat <<EOF
 apiVersion: argoproj.io/v1alpha1
 kind: ArgoCD
 metadata:
@@ -16,12 +16,10 @@ metadata:
   namespace: ${ARGO_NS}
 spec:
   resourceCustomizations: |
-    cdi.kubevirt.io/DataVolume:
-      health.lua: |
-${DV_YAML}
-    kubevirt.io/VirtualMachine:
-      health.lua: |
-${VM_YAML}
 EOF
 
-echo "$YAML"
+for lua in *.lua
+do
+  echo $lua | awk -F_ '{print "    " $1 "/" $2 ": |\n      " $3 ": |" }'
+  sed 's/^/        /' $lua
+done
