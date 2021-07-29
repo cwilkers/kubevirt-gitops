3df852910427dce01fc047098741c3cf13f8bd23
cwilkers <cwilkers@redhat.com>
2021-07-29 21:15:07 +0000

    Updated script to delete app when done

--- win2k19/config/install.sh
+++ win2k19/config/install.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-cd /scripts
-
 echo "Starting install script"
+cd /scripts
 
 oc apply -f windows-install-vm.yaml
 echo "Applied VM, waiting for VM to start"
-
 sleep 5
-
 vm_ready=$(oc get vm windows-install -o jsonpath='{.status.ready}')
 while [ "$vm_ready" != "true" ]
 do
@@ -16,10 +13,8 @@ do
     vm_ready=$(oc get vm windows-install -o jsonpath='{.status.ready}')
 done
 
-echo "VM is started"
-
+echo "VM is started. Waiting for VMI to finish successfully."
 vmi_phase=$(oc get vmi windows-install -o jsonpath='{.status.phase}')
-
 while [ "$vmi_phase" != "Succeeded" ]
 do
     sleep 30
@@ -27,15 +22,11 @@ do
 done
 
 echo "VM has finished installing"
-
 oc apply -f clone-boot-source.yaml
-
 echo "Applied DataVolume to clone boot source image"
 
 sleep 5
-
 dv_phase=$(oc -n openshift-virtualization-os-images get dv win2k19 -o jsonpath='{.status.phase}')
-
 while [ "$dv_phase" != "Succeeded" ]
 do
     sleep 10
@@ -44,5 +35,8 @@ do
 done
 
 echo "Cleaning up"
-
 oc delete -f windows-install-vm.yaml
+
+my_app_name=$(oc get cm windows-install-scripts -o jsonpath='{.metadata.labels.app\.kubernetes\.io/instance}')
+
+oc delete -n openshift-gitops app $my_app_name
