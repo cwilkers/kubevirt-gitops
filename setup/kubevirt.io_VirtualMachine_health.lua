f556d0cb4d3186c4ed9db58d79e0950fd8ff286c
cwilkers <cwilkers@redhat.com>
2021-09-09 16:49:18 -0500

    Fix

--- setup/kubevirt.io_VirtualMachine_health.lua
+++ setup/kubevirt.io_VirtualMachine_health.lua
@@ -1,7 +1,7 @@
 hs = { status="Progressing", message="No status available"}
 if obj.status ~= nil then
   if obj.status.created ~= nil then
-    message="VMI Created"
+    hs.message="VMI Created"
     if obj.status.conditions ~= nil then
       for i, condition in ipairs(obj.status.conditions) do
         if condition.type == "Ready" and condition.status == "True" then
