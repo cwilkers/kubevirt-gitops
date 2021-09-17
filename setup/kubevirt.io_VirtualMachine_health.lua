a504cb93da967e201f3b56514e96e4dbb61ab100
cwilkers <cwilkers@redhat.com>
2021-09-17 17:24:13 -0500

    Script updates from PR

--- setup/kubevirt.io_VirtualMachine_health.lua
+++ setup/kubevirt.io_VirtualMachine_health.lua
@@ -1,17 +1,18 @@
 hs = { status="Progressing", message="No status available"}
 if obj.status ~= nil then
+  if obj.status.ready ~= nil then
+    hs.status = "Healthy"
+    hs.message = "Running"
+  elseif obj.status.created ~= nil then
+    hs.message = "Starting"
+  end
   if obj.status.printableStatus ~= nil then
     hs.message = obj.status.printableStatus
-    if hs.message == "Running" then
-      hs.status = "Healthy"
-    elseif hs.message == "Stopped" or hs.message == "Paused" then
+    if hs.message == "Stopped" or hs.message == "Paused" then
       hs.status = "Suspended"
     elseif hs.message == "Unknown" then
       hs.status = "Degraded"
     end
-  elseif obj.status.ready ~= nil then
-    hs.status = "Healthy"
-    hs.message = "Running"
   end
 end
 return hs
