749d0e722492f3e9c69c861cfd10bd5fe53acdd4
cwilkers <cwilkers@redhat.com>
2021-09-20 12:32:13 -0500

    Ensure script has right path

--- setup/change-repo.sh
+++ setup/change-repo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+MYPATH=$(cd "$(dirname "$0")" && pwd)
+REPOPATH=$(dirname $MYPATH)
+
 GITREPO=$(git remote -v | awk -F '[:/ ]' '/origin.*push/ {print $(NF-2) "/" $(NF-1)}')
 
 # changing     repoURL: https://github.com/cwilkers/kubevirt-gitops
 
-sed -i "s@repoURL: https://github.com/.*@repoURL: https://github.com/${GITREPO}@" ../*/application.yaml
+sed -i "s@repoURL: https://github.com/.*@repoURL: https://github.com/${GITREPO}@" ${REPOPATH}/*/application.yaml
--- setup/install.sh
+++ setup/install.sh
@@ -2,6 +2,8 @@
 
 MYPATH=$(cd "$(dirname "$0")" && pwd)
 
+${MYPATH}/change-repo.sh
+
 A=($(kubectl get argocd -A | tail -n 1) )
 export ARGO_NS=${A[0]}
 export ARGO_CR=${A[1]}
