70b15f7869d151fe2b759ec23da3a18ef56fefa1
cwilkers <cwilkers@redhat.com>
2021-11-01 13:00:19 -0500

    Updated Lua scripts from argocd PR

--- setup/kubevirt.io_VirtualMachineInstance_health.lua
+++ setup/kubevirt.io_VirtualMachineInstance_health.lua
@@ -2,18 +2,33 @@ hs = { status="Progressing", message="No status available"}
 if obj.status ~= nil then
   if obj.status.phase ~= nil then
     hs.message = obj.status.phase
-    if hs.message == "Running" then
-      hs.status = "Healthy"
-    elseif hs.message == "Failed" then
+    if hs.message == "Failed" then
       hs.status = "Degraded"
+      return hs
+    elseif hs.message == "Pending" or hs.message == "Scheduling" or hs.message == "Scheduled" then
+      return hs
     elseif hs.message == "Succeeded" then
       hs.status = "Suspended"
+      return hs
+    elseif hs.message == "Unknown" then
+      hs.status = "Unknown"
+      return hs
     end
-  elseif obj.status.conditions ~= nil then
+  end
+  if obj.status.conditions ~= nil then
     for i, condition in ipairs(obj.status.conditions) do
-      if condition.type == "Ready" and condition.status == "True" then
-        hs.status = "Healthy"
-        hs.message = "Running"
+      if condition.type == "Ready" then
+        if condition.status == "True" then
+          hs.status = "Healthy"
+          hs.message = "Running"
+        else
+          hs.status = "Degraded"
+          hs.message = condition.message
+        end
+      elseif condition.type == "Paused" and condition.status == "True" then
+        hs.status = "Suspended"
+        hs.message = condition.message
+        return hs
       end
     end
   end
--- setup/kubevirt.io_VirtualMachine_health.lua
+++ setup/kubevirt.io_VirtualMachine_health.lua
@@ -1,18 +1,29 @@
 hs = { status="Progressing", message="No status available"}
 if obj.status ~= nil then
-  if obj.status.ready ~= nil then
-    hs.status = "Healthy"
-    hs.message = "Running"
-  elseif obj.status.created ~= nil then
-    hs.message = "Starting"
+  if obj.status.conditions ~= nil then
+    for i, condition in ipairs(obj.status.conditions) do
+      if condition.type == "Paused" and condition.status == "True" then
+        hs.status = "Suspended"
+        hs.message = "Paused"
+        return hs
+      end
+      if condition.type == "Ready" then
+        if condition.status == "True" then
+          hs.status="Healthy"
+          hs.message="Running"
+        else
+          if obj.status.created then
+	    hs.message = "Starting"
+          else
+            hs.status = "Suspended"
+            hs.message = "Stopped"
+          end
+        end
+      end
+    end
   end
   if obj.status.printableStatus ~= nil then
     hs.message = obj.status.printableStatus
-    if hs.message == "Stopped" or hs.message == "Paused" then
-      hs.status = "Suspended"
-    elseif hs.message == "Unknown" then
-      hs.status = "Degraded"
-    end
   end
 end
 return hs
