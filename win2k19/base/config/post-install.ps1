8760eaab7a38464d294e53c5fb79fb71d8f6b9b6
cwilkers <cwilkers@redhat.com>
2022-05-24 16:37:36 -0500

    Strip post to bare bones
    Signed-off-by: cwilkers <cwilkers@redhat.com>

--- win2k19/base/config/post-install.ps1
+++ win2k19/base/config/post-install.ps1
@@ -3,8 +3,8 @@ $BasePath = "C:\Windows\Temp\Install"
 New-item $BasePath -itemtype directory
 
 # Enable RDP
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+#Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+#Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 # Add RedHat to Trusted Publisher
 $CertName = "balloon.cer"
@@ -26,18 +26,18 @@ msiexec /i e:\guest-agent\qemu-ga-x86_64.msi /qn /passive
 Start-Process  E:\vioserial\2k19\amd64\vioser.inf -Verb install
 
 # Enable SSH
-Add-WindowsCapability -Online -Name 'OpenSSH.Server~~~~0.0.1.0'
-Set-Service -Name sshd -StartupType 'Automatic'
+#Add-WindowsCapability -Online -Name 'OpenSSH.Server~~~~0.0.1.0'
+#Set-Service -Name sshd -StartupType 'Automatic'
 
 # Enable Networking
 
-Enable-NetAdapter -Name "Ethernet" -Confirm:$false
+#Enable-NetAdapter -Name "Ethernet" -Confirm:$false
 
 # Get Cloud-init
-Set-ExecutionPolicy Unrestricted
-$Cloudinit = "CloudbaseInitSetup_Stable_x64.msi"
-$CloudinitLocation =  Join-Path -Path $BasePath -ChildPath $Cloudinit
-invoke-webrequest https://cloudbase.it/downloads/$Cloudinit -o $CloudinitLocation
+#Set-ExecutionPolicy Unrestricted
+#$Cloudinit = "CloudbaseInitSetup_Stable_x64.msi"
+#$CloudinitLocation =  Join-Path -Path $BasePath -ChildPath $Cloudinit
+#invoke-webrequest https://cloudbase.it/downloads/$Cloudinit -o $CloudinitLocation
 
 #cmd /C start /wait msiexec /i $CloudinitLocation /qn
 
@@ -58,6 +58,5 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogo
 # Run Sysprep and Shutdown
 
 #cmd /C 'cd "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\" && C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:Unattend.xml'
-#cmd /C 'C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown'
-
-Stop-Computer
+cmd /C 'C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown'
+#Stop-Computer
